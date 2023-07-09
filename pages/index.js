1badc2fd3fbc7eebff81e9495fae8179116f2db5
yashmeet-coder <meetyash681@gmail.com>
2023-07-09 21:44:03 +0530

    added

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useEffect, useState } from 'react'
-import { WhatsappShareButton,WhatsappIcon } from 'react-share'
+import { WhatsappShareButton,WhatsappIcon,FacebookIcon,FacebookShareButton,TwitterIcon,TwitterShareButton } from 'react-share'
 
 const inter = Inter({ subsets: ['latin'] })
 
